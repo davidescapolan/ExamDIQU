63a978d2140cc6751470833f055c3d0c7689fd85
davidescapolan <davide.scapolan@stud.tecnicosuperiorekennedy.it>
2022-07-09 13:06:19 +0200

    Creato progetto MPLABX per il programma del uC

---
+++ uCProgram/EsameFinaleITS_DIQU.X/EsameFinaleMain.c
@@ -0,0 +1,13 @@
+/*
+ * File:   EsameFinaleMain.c
+ * Author: Davide
+ *
+ * Created on 9 luglio 2022, 13.04
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ uCProgram/EsameFinaleITS_DIQU.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=EsameFinaleITS_DIQU.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/EsameFinaleITS_DIQU.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=esamefinaleitsdiqu.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/esamefinaleitsdiqu.x.tar
