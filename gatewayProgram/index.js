a1f2698f5b32c095bc95b4665271f2cbd1c6d9a8
davidescapolan <davide.scapolan@stud.tecnicosuperiorekennedy.it>
2022-07-09 14:46:03 +0200

    Creato Index.js Parte già con una sezione per inizializzare la comunicazione seriale e in scaolto del device twin

---
+++ gatewayProgram/index.js
@@ -0,0 +1,59 @@
+//#region require
+const SerialPort = require('serialport');
+var Protocol = require('azure-iot-device-mqtt').Mqtt;
+var Client = require('azure-iot-device').Client;
+const dotenv = require('dotenv');
+//#endregion
+
+const port = new SerialPort('COM2', {
+  baudRate: 115200
+})
+
+port.open(function (err) {
+    if (err) {
+        return console.log('Error opening port: ', err.message);
+    }
+    port.write('COM2 Open');
+})
+
+// Open errors will be emitted as an error event
+port.on('error', function(err) {
+    console.log('Error: ', err.message);
+})
+
+dotenv.config();
+var deviceConnectionString = process.env.IOTHUB_DEVICE_CONNECTION_STRING;
+
+// create the IoTHub client
+var client = Client.fromConnectionString(deviceConnectionString, Protocol);
+console.log('got client');
+
+// connect to the hub
+client.open(function(err) {
+    if (err) {
+        console.error('could not open IotHub client');
+    }  else {
+        console.log('client opened');
+  
+        // Create device Twin
+        client.getTwin(function(err, twin) {
+            if (err) {
+                console.error('could not get twin');
+            } else {
+                console.log('twin created');
+  
+
+                twin.on('properties.desired', function(delta) {
+                    console.log('new desired properties received:');
+                    console.log(JSON.stringify(delta));
+                    port.write('main screen turn on', function(err) {
+                        if (err) {
+                            return console.log('Error on write: ', err.message);
+                        }
+                        console.log('message written');
+                    });
+                });
+            }
+        });
+    }
+});
